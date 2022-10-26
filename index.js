625cb90daf49b1692d2ad5de0eddc28c90fd145a
Leah-Dirienzo <Leah-Dirienzo@users.noreply.github.com>
2022-10-25 20:05:13 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let home = 0
+let guest = 0
+
+function home1(){
+    home += 1
+    homeScore.textContent = home
+}
+
+function home2() {
+    home += 2
+    homeScore.textContent = home
+}
+
+function home3() {
+    home += 3
+    homeScore.textContent = home
+}
+
+
+function guest1() {
+    guest += 1
+    guestScore.textContent = guest
+}
+
+function guest2() {
+    guest += 2
+    guestScore.textContent = guest
+}
+
+function guest3() {
+    guest += 3
+    guestScore.textContent = guest
+}
